fdf64dd002f3fd882fac4743943b914222a63da6
nelsonlai <nelsonlai9503@gmail.com>
2022-07-15 15:45:33 +0800

    add mysql

---
+++ mysql/mysql.go
@@ -0,0 +1,20 @@
+package mysql
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+// ConnectMySQL connects to MySQL database
+func DB(host string, port string, username string, password string, database string) *gorm.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Silent)})
+	if err != nil {
+		log.Println(err)
+	}
+	return db
+}
